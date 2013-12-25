5213ed31954d563d5766a31289bfc399729669ac
John Hu <huziyong@gmail.com>
2013-12-25 22:34:29 +0800

    rake db:migrate, close #9

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131225123138) do
+ActiveRecord::Schema.define(version: 20131225142843) do
 
   create_table "ideas", force: true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
