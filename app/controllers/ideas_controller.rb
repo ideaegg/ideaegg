3bf5979090d38e5da29d80da1c81388d896d1991
John Hu <huziyong@gmail.com>
2014-01-12 17:22:21 +0800

    Add idea show view

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,6 @@
 class IdeasController < ApplicationController
-  before_action :signed_in_user, only: [:new, :create, :destroy]
+  before_action :set_idea, only: [:show]
+  before_action :signed_in_user, only: [:new, :create, :destroy, :show]
 
   def index
     @ideas = Idea.all
@@ -21,7 +22,15 @@ class IdeasController < ApplicationController
     end
   end
 
-  private 
+  def show
+  end
+
+  private
+
+    def set_idea
+      @idea = Idea.find(params[:id])
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def idea_params
       params.require(:idea).permit(:title, :description)
