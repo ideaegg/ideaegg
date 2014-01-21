1be5a7f56c16593c367f7f6cd3a1ff7f2780c74e
John Hu <huziyong@gmail.com>
2014-01-21 09:41:14 +0800

    Add model test for comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,29 @@
 require 'spec_helper'
 
 describe Comment do
+  let(:user) { Fabricate(:user) }
+  let(:idea) { Fabricate(:idea) }
+
+  subject { Fabricate(:comment) }
+
+  describe 'Respond to' do
+    it { should respond_to(:content) }
+    it { should respond_to(:user_id) }
+    it { should respond_to(:idea_id) }
+  end
+
+  it { should be_valid }
+
+  describe 'Attributes' do
+    let(:comment) do
+      idea.comments.build(content: "Test", user_id: user.id)
+    end
+
+    context '#content' do
+      it 'is not valid with blank content' do
+        comment.content = " "
+        comment.should_not be_valid
+      end
+    end
+  end
 end
