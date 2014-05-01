09263f0e5d76e86b08212048c4757c31d779fe1b
John Hu <huziyong@gmail.com>
2014-05-01 14:58:47 +0800

    Add home page, show ideas as carousel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ideaegg::Application.routes.draw do
+  get "static_pages/home"
   get "notifications/index"
   match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
   delete '/log_out' => 'sessions#destroy', as: :log_out
