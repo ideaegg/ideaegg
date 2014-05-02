194e56c1b44f1d2dbb2db029ce6c5d3679beda8f
John Hu <huziyong@gmail.com>
2014-05-02 11:41:34 +0800

    Resize slide iamges' height

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-  	ids = [3,20,38] 
+  	ids = [42,59,64] 
   	ideas = Idea.find_all_by_id(ids)
   	@ideas = ids.map {|id| ideas.detect{|x| x.id == id}}
   end
